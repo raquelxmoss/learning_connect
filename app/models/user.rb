8e6433ec0f360f55489d461cd2ce1f79a880678b
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-23 16:52:47 +1200

    Create relations for the connection model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  has_many :learner_connections, class_name: 'Connection', foreign_key: 'tutor_id'
+  has_many :learners, class_name: 'User', through: :learner_connections
+
+  has_many :tutor_connections, class_name: 'Connection', foreign_key: 'learner_id'
+  has_many :tutors, class_name: 'User', through: :tutor_connections
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
   devise_for :users
-root to: "pages#welcome"
+  root to: "pages#welcome"
 
 end
