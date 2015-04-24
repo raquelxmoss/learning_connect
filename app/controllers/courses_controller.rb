ed3ebd4e97b8c00f3f924807d4e9e4058c4f573b
Raquel Moss <raquelxmoss@gmail.com>
2015-04-25 10:41:03 +1200

    Add view course partial

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,7 +4,12 @@ class CoursesController < ApplicationController
   end
 
   def create
-    @course = Course.new(connection_id:params[:connection_id], learner_id:params[:learner_id], tutor_id:params[:tutor_id], title:params[:title], price:params[:price])
+    @course = Course.new(connection_id:params[:connection_id], learner_id: User.find(params[:Learner]).id, tutor_id: User.find(params[:Tutor]).id, title:params[:title], price:params[:price])
+    if @course.save
+      redirect_to :back
+    else
+      #add an error message
+    end
   end
 
   def new
