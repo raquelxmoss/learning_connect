bde8408f5a27a99fc44877cdccd92793d1596df9
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-24 15:07:07 +1200

    change user has many courses to user has many lessons and classes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe User, type: :model do
 
   it {should have_many :connections}
   it {should have_many :acquaintances}
-  it {should have_many :courses}
+  it {should have_many :classes}
+  it {should have_many :lessons}
   it {should have_many :tutors}
   it {should have_many :learners}
 
