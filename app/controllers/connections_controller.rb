7cf5b7ea8481cf5be16495006bf6945b070b88fa
Raquel Moss <raquelxmoss@gmail.com>
2015-04-24 20:59:39 +1200

    Add messages to connection view

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -14,7 +14,7 @@ class ConnectionsController < ApplicationController
   end
 
   def show
-    @connection = Connection.find(params[:connection_id])
+    @connection = Connection.find(params[:id])
   end
 
   def new
