657e146b7e2db70d4f3ccc2aa8d7aafb4d6211ef
johnvirtuosity <john.kan@virtuosity.co.nz>
2015-04-25 11:28:22 +1200

    Complete skill seeding and delete action of things to teach and things to learn from user profile page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,7 @@
 class SkillsController < ApplicationController
-
 	def destroy
 		skill = Skill.find(params[:id])
 		skill.destroy()
+		redirect_to user_path(params[:user_id])
 	end
 end
--- db/seeds/skills.rb
+++ db/seeds/skills.rb
@@ -1,9 +1,19 @@
 def create_skills users
 
   Skill.delete_all
-
+  
   # code in here !
+  users.each do |user|  	
+  	rand(1..5).times do
+  		user.skills << Skill.create( description: "to  #{Faker::Hacker.verb} things", skill_type: 'teach' )
+  	end
+  end
 
+  users.each do |user|  	
+  	rand(1..5).times do
+  		user.skills << Skill.create( description: "to  #{Faker::Hacker.verb} stuff", skill_type: 'learn' )
+  	end
+  end
 
   puts "#{Skill.count} skills created"
 
