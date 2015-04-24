d349cbc727b80ff14d4acff08f7505222e8c5e1d
Raquel Moss <raquelxmoss@gmail.com>
2015-04-25 09:26:54 +1200

    Add price to course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,13 @@
 class CoursesController < ApplicationController
+
+  def show
+  end
+
+  def create
+    @course = Course.new(connection_id: , learner_id: , tutor_id:, title:, status: )
+  end
+
+  def new
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424071613) do
+ActiveRecord::Schema.define(version: 20150424212510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150424071613) do
     t.string   "title"
     t.string   "status",        default: "pending"
     t.integer  "connection_id"
+    t.integer  "price"
   end
 
   create_table "learning_objectives", force: :cascade do |t|
