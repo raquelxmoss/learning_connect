75c1c8eb28d937f91a7cb0da4acfdf9745dd5464
Raquel Moss <raquelxmoss@gmail.com>
2015-04-24 16:15:21 +1200

    Add connection

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,10 +3,10 @@ class ConnectionsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @initializer = current_user
-    @receiver = User.find(params[:receiver]) #this is the user whose profile page they are looking at
-    @connection = Connection.new(receiver_id: @receiver, initializer_id: @initializer)
-    if @connection.save(receiver_id: @receiver, initializer_id: @initializer)
+    @initializer = current_user.id
+    @receiver = User.find(params[:receiver_id]) #this is the user whose profile page they are looking at
+    @connection = Connection.new(receiver_id: @receiver.id, initializer_id: @initializer)
+    if @connection.save
       redirect_to connections_path
     else
       render users_path(@receiver)
@@ -17,6 +17,9 @@ class ConnectionsController < ApplicationController
   end
 
   def new
+    @initializer = current_user.id
+    @receiver = User.find(params[:id]) #this is the user whose profile page they are looking at
+    @connection = Connection.new(receiver_id: @receiver.id, initializer_id: @initializer)
   end
 
   def edit
