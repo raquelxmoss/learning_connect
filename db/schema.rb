86c7e0563032c31fff6e459c3544a0cbb5ec014a
Raquel Moss <raquelxmoss@gmail.com>
2015-04-24 14:58:10 +1200

    basic sign up style done

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20150424013637) do
     t.integer  "learner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "course_id"
   end
 
   create_table "courses", force: :cascade do |t|
