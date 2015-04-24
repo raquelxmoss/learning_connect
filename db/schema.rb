ff217ae949ee4b9af34c48ab7e7267d8e1b84c91
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-24 14:25:03 +1200

    rename tutor and learner to receiver and initializer

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424021922) do
+ActiveRecord::Schema.define(version: 20150424022258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "connections", force: :cascade do |t|
-    t.integer  "tutor_id"
-    t.integer  "learner_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "initializer_id"
+    t.integer  "receiver_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "courses", force: :cascade do |t|
