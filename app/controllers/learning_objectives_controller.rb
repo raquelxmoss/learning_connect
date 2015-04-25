ba6f5dca19aa76d4557357fc4163c6a49d01bdba
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-25 13:35:11 +1200

    create get_course and before_filter

--- app/controllers/learning_objectives_controller.rb
+++ app/controllers/learning_objectives_controller.rb
@@ -1,2 +1,14 @@
 class LearningObjectivesController < ApplicationController
+  before_filter :get_course
+
+
+
+
+  private
+
+  def get_course
+    @course = Course.find(params[:course_id])
+    @learning_objectives = @course.learning_objectives
+  end
+
 end
