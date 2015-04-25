a19aa333626c102e6ef8a4b3f33bd8e3fca29270
Raquel Moss <raquelxmoss@gmail.com>
2015-04-26 09:17:00 +1200

    Change ajax request to reflect new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
- 
+
 
   devise_for :users
 
-  resources :connections, only: [:create, :show, :new, :destroy] do 
-    resources :courses do 
+  resources :connections, only: [:create, :show, :new, :destroy] do
+    resources :courses do
       resources :learning_objectives, only: [:show, :index, :destroy, :create]
       resources :ratings, only: [:show, :index, :destroy, :create]
     end
     resources :messages
   end
 
-
+  delete '/connections/:connection_id/messages/:id' => 'messages#destroy', :as => 'delete_message'
   get '/users/:id', to: 'users#show'
   get '/users', to: 'users#index'
 
