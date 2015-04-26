5a81f3adce9ce29cb1e0aa70afc4027bb1232171
Raquel Moss <raquelxmoss@gmail.com>
2015-04-26 17:30:32 +1200

    ajaxified adding skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,11 +1,14 @@
 class SkillsController < ApplicationController
-	
+
   before_filter :get_user
 
 	def create
 		@skill = Skill.new(skill_params)
 		if @skill.save(skill_params)
-		  redirect_to user_path(params[:user_id])
+		  # redirect_to user_path(params[:user_id])
+      render partial: 'show', layout: false
+    else
+      render json: @skill, status: :unprocessable_entity
 		end
 	end
 
