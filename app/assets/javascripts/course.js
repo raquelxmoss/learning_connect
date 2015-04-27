f00736a9d477e2cd746a23b1f968c33358300b61
Raquel Moss <raquelxmoss@gmail.com>
2015-04-27 17:11:58 +1200

    ratings ajaxified

---
+++ app/assets/javascripts/course.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $(".rating-button").click(function(e){
+     e.preventDefault();
+    var url =$(this).parent().attr("action");
+    var data = $(this).parent().serializeArray();
+    $.ajax({
+     url:url,
+     method:"POST",
+     data:data,
+     success:function(res){
+      $("#lesson-rating-form").remove();
+      $("#ratings").append(res);
+     },
+     error:function(){
+      alert("Could not create rating")
+     }
+    });
+  });
+});
---
+++ app/views/ratings/untitled.js
@@ -0,0 +1,20 @@
+var map,latLng,marker,infoWindow,ad;function initialize(){
+  var myOptions={
+    zoom:16,
+    panControl:false,
+    streetViewControl:true,
+    scaleControl:true,
+    zoomControl:true,
+    zoomControlOptions:{
+      style:google.maps.ZoomControlStyle.LARGE,position:google.maps.ControlPosition.LEFT_CENTER},
+      scaleControlOptions:{position:google.maps.ControlPosition.BOTTOM_RIGHT},
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('googlemaps'),myOptions);
+  if(navigator.geolocation) {
+    defaultLocation();
+    navigator.geolocation.getCurrentPosition(locationFound,defaultLocation);
+  }else{
+    defaultLocation();
+  }
+}
