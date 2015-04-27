42128d45f43ea7f34790e7042a9cd9165b3479c3
Raquel Moss <raquelxmoss@gmail.com>
2015-04-28 10:29:25 +1200

    add route and controller method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def feed
+    @all_connections = Connection.limit(10).order('created_at DESC')
+    @all_courses = Course.limit(10).order('created_at DESC')
+    @new_users = User.limit(10).order('created_at DESC')
+    @feed_items = (@all_connections + @all_courses + @new_users).sort_by &:created_at
+    @feed_items.reverse!
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+
+  def render_feed_item(item)
+    #if feed item type = User
+    #render user feed partial
+    #elsif feed item type = Connection
+    #render feed item partial
+    #elsif feed itme type = Course
+    #render feed item for course
+  end
 end
