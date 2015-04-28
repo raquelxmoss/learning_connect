8d608e17956e81e50bdc09eaf89697f64e5fe19c
Raquel Moss <raquelxmoss@gmail.com>
2015-04-29 11:40:52 +1200

    added a method to controller, please don't touch it :-)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def show
     get_user
     get_user_connections
+    get_user_connection
     @skills = @user.skills
   end
 
@@ -24,6 +25,10 @@ class UsersController < ApplicationController
     if current_user
       @connections = @user.initializer_connections + @user.receiver_connections
     end
+  end
+
+  def get_user_connection
+    @connection = Connection.find_by(receiver_id: current_user.id, initializer_id: params[:id]) || Connection.find_by(receiver_id: params[:id], initializer_id: current_user.id)
   end
 
 end
