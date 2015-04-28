d014348331d4bd60d3cb6be5cfe4113b065ce0c7
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-28 20:45:25 +1200

    Test search scope on Skill model

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -4,4 +4,40 @@ RSpec.describe Skill, type: :model do
 
   it {should belong_to :user}
 
+
+
+  describe 'SCOPE #search' do 
+    let(:spanish){create(:skill_spanish)}
+    let(:english){create(:skill_english)}
+
+    context 'matching results' do 
+      before{ @skills =  Skill.search('sp') }
+
+      it 'returns the skills with matching name' do 
+        expect(@skills).to include(spanish)
+      end
+
+      it 'does not return skills with unmatching name' do 
+        expect(@skills).not_to include(english)
+      end
+    end
+
+    context 'unmatching result names' do 
+      before{ @skills =  Skill.search('xzz') }
+
+      it 'returns an empty array' do 
+        expect(@skills).to eq([])
+      end
+    end
+
+    context 'no argument passed in' do 
+      before{ @skills =  Skill.search(nil) }
+
+      it 'returns all the skills' do
+        expect(@skills.count).to eq(Skill.count)
+      end
+    end
+
+  end
+
 end
