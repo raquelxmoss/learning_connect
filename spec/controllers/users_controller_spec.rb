18c330bba61449f52637d98d5e75046dc7c68dde
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-28 16:48:44 +1200

    minor changes on users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,32 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  
-  
+  # before(:each) do 
+  #   @user = create(:user)
+  #   sign_in @user 
+  # end
+  describe 'GET' do
+
+    before do 
+      @user = create(:user)
+    end
+
+    context "#index" do
+      it "assigns all users as @users" do
+        get :index, {}
+        expect(assigns(:users)).to eq([@user])
+      end
+    end
+
+    context "#show" do
+      it "assigns the requested user as @user" do
+        get :show, {:id => @user.to_param}
+        expect(assigns(:user)).to eq(@user)
+      end
+    end
+
+    after(:each) { User.delete_all }
+
+  end
+
 end
