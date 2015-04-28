f533fbbc8d32f29e33f79342c7b808b99ff83001
Raquel Moss <raquelxmoss@gmail.com>
2015-04-28 16:37:28 +1200

    Feature test for Create Lesson

--- spec/features/user_can_create_course_spec.rb
+++ spec/features/user_can_create_course_spec.rb
@@ -5,23 +5,38 @@ feature "Create course", js: true do
   background do
       @tutor = create(:user)
       @learner = create(:user)
+      @connection = Connection.create(initializer_id:@tutor.id, receiver_id: @learner.id)
       sign_in_as @tutor
-      visit user_path(@learner)
-      click_on 'Connect'
-      visit user_path(@tutor)
-    end
-
-    background do
-      @user = FactoryGirl.create(:user, password: 'password')
-      visit '/'
-      click_link 'Login'
-      fill_in 'user_email', with: @user.email
+      visit connection_path(@connection)
     end
 
   scenario "visit connection page" do
-    visit user_path(@tutor)
-    click_on("#{@learner.name}")
     expect(current_path).to eq(connection_path(@connection))
   end
 
+  scenario "click Create a lesson" do
+    expect(current_path).to eq(connection_path(@connection))
+    expect(page).to have_content("Create a lesson")
+    click_on("Create a lesson")
+    page.has_css?('#new-lesson-form', :visible => true)
+  end
+
+  scenario "Create lesson form creates a lesson" do
+    click_on("Create a lesson")
+    fill_in('Title', :with => 'New Lesson')
+    fill_in('Price', :with => '20')
+    select(@tutor.name, :from => 'Tutor')
+    fill_in('Length', :with => '1')
+    click_on("Create Course")
+    click_on("All Lessons")
+    expect(page).to have_content("New Lesson")
+  end
+
+  scenario "click Add Learning Objective" do
+    click_on("Create a lesson")
+    click_on("Add a Learning Objective")
+    page.has_css?('#new-learning-objectives', :visible => true)
+  end
+
 end
+
