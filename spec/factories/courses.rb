1c92d325d910c521e0ac507e5221ee294f6c72ba
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-28 16:48:21 +1200

    rake db:drop and fix factory bug on courses

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,11 +1,6 @@
-connection = Connection.all.sample
-
 FactoryGirl.define do
   factory :course do
-    connection_id {connection.id}
     title {Faker::Lorem.sentence}
     status {Faker::Lorem.paragraph}
-    tutor {connection.receiver.id}
-    learner {connection.initializer.id}
   end
 end
