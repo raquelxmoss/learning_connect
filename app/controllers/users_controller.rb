e738a2c145cc61e2217e958718e9db62bb778d90
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-28 14:19:58 +1200

    remove more unused methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,23 +21,6 @@ class UsersController < ApplicationController
     @teaching_skills = @user.skills.teaching_skills
   end
 
-  def edit
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save(user_params)
-      redirect_to users_path
-    else
-      render 'new'
-    end
-  end
-
-  def new
-    @user = User.new(user_params)
-    render 'new'
-  end
-
   def list
     # @users = User.all
     if params[:search]
