719235fa936186a1ac6a1ed8ce99638ad74ffdf5
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-29 14:37:27 +1200

    validate uniqueness of receiver and intializer

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Connection, type: :model do
   it {should belong_to :initializer}
   it {should belong_to :receiver}
   it {should have_many :messages}
+  it {should validate_uniqueness_of :initializer_id}
+  it {should validate_uniqueness_of :receiver_id}
 
   describe 'Aggrement reached' do 
 
