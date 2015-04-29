45a6d5841f7422a7af31919db270811e7a8857d6
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-29 15:46:03 +1200

    Test connections controller GET #show

--- spec/controllers/connections_controller_spec.rb
+++ spec/controllers/connections_controller_spec.rb
@@ -2,8 +2,58 @@ require 'rails_helper'
 
 RSpec.describe ConnectionsController, type: :controller do
 
+  let(:receiver) {create(:user)}
+  let(:initializer) {create(:user)}
+  let(:matching_connection) {create(:connection, receiver: receiver, initializer: initializer)}
+  let(:course) {matching_connection.courses.create}
+  let(:message) {matching_connection.messages.create}
+  let(:unmatching_connection) {create(:connection)}
 
-  
+  describe 'GET' do 
+
+    describe '#show' do 
+      subject{ get :show,{id: unmatching_connection.id} }
+
+      context 'when not signed_in' do
+        it 'redirects to the login page' do
+          expect(subject).to redirect_to(new_user_session_path)
+        end
+      end
+
+      context 'when signed in' do 
+        before { sign_in receiver }
+
+        context 'Not current_user connections' do 
+          it 'redirects to user profie page' do 
+            expect(subject).to redirect_to(user_path receiver)
+          end
+        end
+
+        context 'current_user connections' do 
+          before { get :show,{id: matching_connection.id} }
+
+          it 'assigns correct users to @users' do 
+            expect(assigns(:users)).to eq([initializer, receiver])
+          end
+          it 'assigns correct courses to @courses' do 
+            expect(assigns(:courses)).to eq([course])
+          end
+          it 'assigns correct messages to @messages' do 
+            expect(assigns(:messages)).to eq([message])
+          end
+          it 'creates a new instance of a course' do 
+            expect(assigns(:course)).to be_a_new(Course)
+          end
+        end
+        
+      end
+
+
+
+    end
+
+  end
+
 
 
 end
