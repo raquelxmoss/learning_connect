f9321d1e8803c5dbc1ca06d985813fb7fc59d97d
Alexandre Barret <barretalexandre@hotmail.com>
2015-04-29 12:37:33 +1200

    add default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  # config.action_mailer.default_url_options = { :host => 'yoursite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'learning-co.herokuapp.com' }
 
   # Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
 
