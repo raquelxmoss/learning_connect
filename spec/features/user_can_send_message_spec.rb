04bacdfe8517a827877c4762552e480cd167e9f9
Raquel Moss <raquelxmoss@gmail.com>
2015-04-29 20:24:42 +1200

    indentation

--- spec/features/user_can_send_message_spec.rb
+++ spec/features/user_can_send_message_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 feature "Send Message", js: true do
 
   background do
-      @tutor = create(:user)
-      @learner = create(:user)
-      @connection = Connection.create(initializer_id:@tutor.id, receiver_id: @learner.id)
-      sign_in_as @tutor
-      visit connection_path(@connection)
-    end
+    @tutor = create(:user)
+    @learner = create(:user)
+    @connection = Connection.create(initializer_id:@tutor.id, receiver_id: @learner.id)
+    sign_in_as @tutor
+    visit connection_path(@connection)
+  end
 
   scenario "Fill in message form" do
     fill_in('content', :with => 'This is a new message right here')
