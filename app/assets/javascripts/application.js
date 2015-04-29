54802efc098fd318aac46e17a9e35444f37f17f3
Raquel Moss <raquelxmoss@gmail.com>
2015-04-29 20:24:50 +1200

    added the js stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,27 @@ $("#add-learning-objective").click(function(e){
  $(".learning-objectives").append($("#new-learning-objectives").html());
  });
 
+ $('.map').click(function (e) {
+    e.preventDefault();
+    // var li = $(this).parent();
+    var id = $(this).data("id");
+
+    var url = ('/map/'+ id);
+    console.log(url);
+    $.ajax({
+      url: url,
+      method:'GET',
+      success:function(res){
+
+      addMarker(res.lat,res.long);
+      console.log(res.lat,res.long);
+      },
+      error: function(req, errorType, errorMessage){
+        alert(req, errorType, errorMessage);
+      }
+    });
+ });
+
 var users = $('.all-users').children();
   for(var i=0; i < users.length; i++){
     if (i%3 == 2) {
@@ -52,6 +73,28 @@ var users = $('.all-users').children();
 
 
  });
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
+
+
+
+
+
 
 
 
